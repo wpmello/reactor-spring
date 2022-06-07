cc3ec6167cbd20b29cb541aca209f3971b1acb44
wpmello <wagnermello903@gmail.com>
2022-06-07 19:12:07 -0300

    Subscribing to Mono

--- src/test/java/reactive/test/MonoTest.java
+++ src/test/java/reactive/test/MonoTest.java
@@ -2,12 +2,27 @@ package reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 @Slf4j
+/*
+    Reactive Streams
+    1. As
+ */
 public class MonoTest {
 
     @Test
-    public void test(){
-        log.info("Everything working as intended");
+    public void monoSubscriber(){
+        String name = "Soares";
+        Mono<String> mono = Mono.just(name).log();
+
+        mono.subscribe();
+
+        log.info("-----------------------");
+        StepVerifier.create(mono)
+                .expectNext(name)
+                .verifyComplete();
+
     }
 }
