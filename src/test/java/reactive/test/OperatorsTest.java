839a3677aabde82f2a1a54bf7b8484deed49685e
wpmello <wagnermello903@gmail.com>
2022-06-08 14:58:57 -0300

    Merge and MergeWith

--- src/test/java/reactive/test/OperatorsTest.java
+++ src/test/java/reactive/test/OperatorsTest.java
@@ -11,6 +11,7 @@ import reactor.test.StepVerifier;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.time.Duration;
 import java.util.List;
 import java.util.Locale;
 import java.util.concurrent.atomic.AtomicLong;
@@ -171,6 +172,10 @@ public class OperatorsTest {
                 .verify();
     }
 
+    private Flux<Object> emptyFlux() {
+        return Flux.empty();
+    }
+
     @Test
     public void deferOperator() throws InterruptedException {
         Mono<Long> just = Mono.just(System.currentTimeMillis());
@@ -242,7 +247,42 @@ public class OperatorsTest {
                 .verify();
     }
 
-    private Flux<Object> emptyFlux() {
-        return Flux.empty();
+    @Test
+    public void mergeOperator() throws InterruptedException {
+        Flux<String> flux1 = Flux.just("a", "b").delayElements(Duration.ofMillis(200));
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeFlux = Flux.merge(flux1, flux2)
+                .delayElements(Duration.ofMillis(200))
+                .log();
+
+        mergeFlux.subscribe(log::info);
+
+        Thread.sleep(1000);
+
+        StepVerifier.create(mergeFlux)
+                .expectSubscription()
+                .expectNext("c", "d","a", "b")
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void mergeWithOperator() throws InterruptedException {
+        Flux<String> flux1 = Flux.just("a", "b").delayElements(Duration.ofMillis(200));
+        Flux<String> flux2 = Flux.just("c", "d");
+
+        Flux<String> mergeWith = flux1.mergeWith(flux2).delayElements(Duration.ofMillis(200))
+                .log();
+
+        mergeWith.subscribe(OperatorsTest.log::info);
+
+        Thread.sleep(1000);
+
+        StepVerifier.create(mergeWith)
+                .expectSubscription()
+                .expectNext("c", "d","a", "b")
+                .expectComplete()
+                .verify();
     }
 }
