1974ffea52c31e14c07bbab847f3f88cd92d1891
wpmello <wagnermello903@gmail.com>
2022-06-08 14:11:28 -0300

    From Callable

--- src/test/java/reactive/test/OperatorsTest.java
+++ src/test/java/reactive/test/OperatorsTest.java
@@ -1,11 +1,17 @@
 package reactive.test;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 @Slf4j
 public class OperatorsTest {
@@ -129,6 +135,24 @@ public class OperatorsTest {
         StepVerifier.create(flux)
                 .expectSubscription()
                 .expectNext(1,2,3,4)
+                .verifyComplete();
+    }
+
+    @Test
+    public void subscribeOnIO() {
+        Mono<List<String>> list = Mono.fromCallable(() -> Files.readAllLines(Path.of("text-test-file")))
+                .log()
+                .subscribeOn(Schedulers.boundedElastic());
+
+//        list.subscribe(strings -> log.info("{}", strings));
+
+        StepVerifier.create(list)
+                .expectSubscription()
+                .thenConsumeWhile(l -> {
+                    Assertions.assertFalse(l.isEmpty());
+                    log.info("Size {}", l.size());
+                    return true;
+                })
                 .verifyComplete();
     }
 }
